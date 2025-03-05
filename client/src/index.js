b18a755b3acaf6616838ccc4c42774df6944ba4b
khajavali <157942120+khajavali8@users.noreply.github.com>
2025-03-05 16:59:00 +0530

    Delete client directory

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // ✅ Use React 18's new API
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
